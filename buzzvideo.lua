a3db8434e8087376141c9b1c04274ae326bc0b1a
arkiver <arkiver@protonmail.com>
2022-12-19 19:27:44 +0100

    Version 20221219.04. Accept status code 400 on topbuzzcdn.com URLs.

--- buzzvideo.lua
+++ buzzvideo.lua
@@ -550,6 +550,11 @@ wget.callbacks.httploop_result = function(url, err, http_stat)
     return wget.actions.EXIT
   end
 
+  if status_code == 400
+    and string.match(url["url"], "^https?://[^/]*topbuzzcdn%.com/") then
+    return wget.actions.EXIT
+  end
+
   if status_code ~= 200 then
     io.stdout:write("Server returned bad response. Sleeping.\n")
     io.stdout:flush()
